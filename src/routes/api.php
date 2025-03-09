225781388e1f224031e1b6e3157db1e40b7cc344
nicolaszp <niolaszp@gmail.com>
2025-03-09 14:14:42 -0600

    PCA-28 auteticacion para los endpoints de entradas e inventario

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,15 +6,13 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\EntradaController;
 use App\Http\Controllers\InventarioController;
 
-Route::post('auth/login', [LoginController::class, 'login']);
+Route::post('login', [LoginController::class, 'login']);
 
-Route::post('auth/logout', [LoginController::class, 'logout']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('logout', [LoginController::class, 'logout']);
 
-Route::post('/entradas', [EntradaController::class, 'procesarEntrada']);
+    Route::post('/entradas', [EntradaController::class, 'procesarEntrada']);
 
-Route::get('/inventario', [InventarioController::class, 'obtenerInventario']);
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    Route::get('/inventario', [InventarioController::class, 'obtenerInventario']);
+});
 

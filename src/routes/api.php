ad603efb2afaf8ac1607c061049e14e0e2044dfe
AlanJvs <al06266@uacam.mx>
2025-03-16 10:25:10 -0600

    Completado el historial de entradas y generacion del PDF

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\EntradaController;
 use App\Http\Controllers\InventarioController;
+use App\Http\Controllers\HistorialEntradaController;
+use App\Http\Controllers\ValeEntradaController;
+
 
 Route::post('auth/login', [LoginController::class, 'login']);
 
@@ -13,6 +16,10 @@ Route::post('auth/logout', [LoginController::class, 'logout']);
 Route::post('/entradas', [EntradaController::class, 'procesarEntrada']);
 
 Route::get('/inventario', [InventarioController::class, 'obtenerInventario']);
+
+Route::get('/historial-entradas', [HistorialEntradaController::class, 'obtenerHistorial']);
+
+Route::get('/vale-entrada/{id_entrada}', [ValeEntradaController::class, 'generarVale']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
